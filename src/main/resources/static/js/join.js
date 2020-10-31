775a95fd4ceaaf2f32f1e7835ea08bdd143358c3
HA <HA@192.168.56.1>
2020-10-31 11:11:50 +0900

    아이디 , pwd 체크

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -5,6 +5,12 @@ $(document).ready( function() {
 		PW_TO_LONG: '패스워드가 너무 깁니다(6~12글자)'
 	}
 	$('#member').on('click', function() {
+		var error = chkPW() // null '비번이....'
+		if(error){
+			alert(error);
+			return ;
+		}
+		
 		//console.log('ok?')
 		var email = $('#email').val();
 		var pwd = $('#pwd').val();
@@ -32,4 +38,60 @@ $(document).ready( function() {
 			}
 		})
 	})
+	
+	function checkForm($input, fnValidator) {
+		// var $input = $(e.target)
+		var $td = $input.parent()
+		var error = fnValidator();
+		if(error){
+			$td.find('.error').remove();
+			$td.append(`<label class="error">${error}</label>`)
+		} else {
+			$td.find('.error').remove();
+		}
+	}
+	$('#pwd').on('input',function(e){
+		// var pw = e.target.value
+		checkForm($(e.target), chkPW)
+	})
+	
+	$('#email').on('input',function(e){
+		checkForm($(e.target), chkEmail)
+	})
 });
+
+function chkPW(){
+
+	var pw = $("#pwd").val();
+	var id = $("#email").val();
+		
+	var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+	var hangulcheck = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+	 
+	if(pw.search(id) > -1){
+		return "비밀번호에 아이디가 포함되었습니다.";
+	} else if(pw.search(/\s/) != -1){
+		return "비밀번호는 공백 없이 입력해주세요.";
+	} else if(false === reg.test(pw)) {
+		return '비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.';
+	} else if(/(\w)\1\1\1/.test(pw)){
+		return '같은 문자를 4번 이상 사용하실 수 없습니다.';
+	}else if(hangulcheck.test(pw)){
+		return "비밀번호에 한글을 사용 할 수 없습니다.";
+	}else {
+		// alert("회원 가입 되었습니다.");
+		return null;
+	}
+}
+
+function chkEmail(){
+	var pw = $("#pwd").val();
+	var id = $("#email").val();
+
+	
+	if(id.indexOf('@') < 0) {
+		return '유효하지 않은 이메일 입니다.'
+	}else{
+		return null;
+	}
+}
